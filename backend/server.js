d39449769e37a4dd5498fec7b9abba665af689d4
Irfan arafath <dk9222547@gmail.com>
2025-10-04 15:46:51 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// mount routes
+// mount routes      
 app.use('/api/Students', StudentRoutes);
 app.use('/api/Teachers', TeacherRoutes);
 app.use('/api/Course', CourseRoutes);
